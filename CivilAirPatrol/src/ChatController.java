6b6b22b72cd6b72ae52b40d3c33a4caad2582ef6
wassenr <rgwassenaar@gmail.com>
2014-12-04 16:19:14 -0800

    created a chatView and implemented part of controller
    at the moment, an actionListener updates large text area when a user
    pressers enter on data entry line, this actionlistener will need to be
    rewritten to handle sending of the message

--- CivilAirPatrol/src/ChatController.java
+++ CivilAirPatrol/src/ChatController.java
@@ -1,4 +1,6 @@
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /* Placeholder for demonstrating Session MVC */
 public class ChatController implements Controller {
@@ -9,11 +11,43 @@ public class ChatController implements Controller {
     public ChatController() {
         this.view = new ChatView();
         this.model = new ChatModel();
+        view.setTextChatArea("");
+        view.setTextChatEntry("");
+        setListeners();
     }
 
     @Override
     public Component getViewComponent() {
         return view;
+    }
+    
+    public void setListeners(){
+        view.addActionListenChatEntry(new ActionListener(){
+            public void actionPerformed(ActionEvent event){
+                //What do i do when the user hits enter
+                //TODO this is where we need to send the chat message to server
+                //instead of just printing to the chatArea
+                String textArea = view.getTextChatArea();
+                String textEntry = view.getTextChatEntry();
+                
+                if (textEntry.isEmpty()) return;               
+                
+                switch (textArea){
+                    case "":
+                        view.setTextChatArea(textEntry);
+                        break;
+                        
+                    default:
+                        view.setTextChatArea(textArea + "\n" + textEntry);
+                }
+                
+         
+                view.setTextChatEntry("");
+                
+                
+            }
+        });   
+        
     }
 
 }
