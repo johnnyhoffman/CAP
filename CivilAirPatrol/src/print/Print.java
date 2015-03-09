7c2fb6dbd628bb22dca899f6c307455241cf1e4e
Dana Vold <danavold@gmail.com>
2015-03-09 13:39:27 -0700

    basic image saving now works, although the UI isn't done

--- CivilAirPatrol/src/print/Print.java
+++ CivilAirPatrol/src/print/Print.java
@@ -12,6 +12,7 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
@@ -28,40 +29,23 @@ import javax.swing.JPanel;
  */
 public class Print {
     
-public static void main(String[] args) {
-    JFrame f = new JFrame();
-    f.setSize(800, 1200);
-    f.setExtendedState(f.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);    
-    
-    RadioMessageView c = new RadioMessageView();
-    
-    f.add(c);
-    f.setVisible(true);
-    //f.setSize(400, 500); // Must set some size, even though it starts
-                             // maximized. Else, un-maximizizing the window
-                             // may make it ~1 pixel wide
-    
-    //f.setExtendedState(f.getExtendedState() | JFrame.MAXIMIZED_BOTH);
-    
-    
-    saveComponentAsJPEG(c, "testComm.jpg");
-}
-    
-public static void saveComponentAsJPEG(Component myComponent, String filename) {
-        System.out.println("printing: "+filename);
-        Dimension size = myComponent.getSize();
-        System.out.println(size.toString());
-        BufferedImage myImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g2 = myImage.createGraphics();
-        myComponent.paint(g2);
-        try {
-            OutputStream out = new FileOutputStream(filename);
-            ImageIO.write(myImage, "jpeg", out);
-            out.close();
-        } catch (Exception e) {
-            System.out.println(e);
-        }
-    }
+	 
+		public static void saveComponentAsJPEG(Component myComponent, String filename) {
+	        System.out.println("printing: "+filename);
+	        //myComponent = myComponent.getComponentAt(2,2);
+	        Dimension size = myComponent.getComponentAt(2,2).getComponentAt(2,2).getSize();
+	        System.out.println(size.toString());
+	        BufferedImage myImage = new BufferedImage(size.width, size.height, BufferedImage.TYPE_INT_RGB);
+	        Graphics2D g2 = myImage.createGraphics();
+	        myComponent.getComponentAt(2,2).getComponentAt(2,2).paint(g2);
+	        try {
+	            OutputStream out = new FileOutputStream(filename);
+	            ImageIO.write(myImage, "jpeg", out);
+	            out.close();
+	        } catch (Exception e) {
+	            System.out.println(e);
+	        }
+	    }
 
-}
+	
+	}
